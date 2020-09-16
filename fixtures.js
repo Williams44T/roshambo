568149b3f5277b4a1f53b99e0cfd4437020cc442
Travis Williams <williams44t@gmail.com>
2020-09-16 11:02:36 -0700

    add new script "fixtures" declared object avatars with all nine images

---
+++ fixtures.js
@@ -0,0 +1,11 @@
+let avatars = {
+  aang: { img: './images/avatars/aang.png' },
+  deku: { img: './images/avatars/deku.png' },
+  goku: { img: './images/avatars/goku.png' },
+  link: { img: './images/avatars/link.png' },
+  luffy: { img: './images/avatars/luffy.png' },
+  naruto: { img: './images/avatars/naruto.png' },
+  sasuke: { img: './images/avatars/sasuke.png' },
+  vegeta: { img: './images/avatars/vegeta.png' },
+  zoro: { img: './images/avatars/zoro.png' }
+};
