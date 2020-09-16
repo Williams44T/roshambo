c5e469ac8f9f2ac2df30d30c57ed97b511f9ed76
Travis Williams <williams44t@gmail.com>
2020-09-16 14:44:02 -0700

    added input box to bottom of player selection

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let input = $('<input>');
+
 let loadStageOne = function() {
   $('#stage').empty();
-  for (let avatar in avatars) {
-    $('#stage').append($('<img>').attr('src', avatars[avatar].img));
-  }
+  // for (let avatar in avatars) {
+  //   $('#stage').append($('<img>').attr('src', avatars[avatar].img));
+  // }
+  input.attr('placeholder', 'name your champion');
+  $('#stage').append(input);
 };
 
 $(document).ready(function() {
