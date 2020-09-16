98bceda6cd8281a4b477409b71621d4a64d3cefa
Travis Williams <williams44t@gmail.com>
2020-09-16 16:09:13 -0700

    added title to stage one

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+let stageTitle = $('<h2></h2>');
 let avatarSelection = $('<div></div');
 let input = $('<input>');
 
 let loadStageOne = function() {
-  $('#stage').empty().append(avatarSelection);
+  $('#stage').empty()
+    .append(stageTitle)
+    .append(avatarSelection)
+    .append(input);
 
   for (let avatar in avatars) {
       avatarSelection.append($('<img>').attr('src', avatars[avatar].img));
   }
 
+  stageTitle.text('Choose Your Champion');
   input.attr('placeholder', 'name your champion');
-  $('#stage').append(input);
 };
 
 $(document).ready(function() {
