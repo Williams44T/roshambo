d6539d1f5d3262630f96b6d525f8b19062338a60
Travis Williams <williams44t@gmail.com>
2020-09-16 17:25:01 -0700

    added helper fn appendScrambledImages

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let avatarSelection = $('<div></div>');
 let input = $('<input>');
 let submit = $('<button>Submit</button>');
 
+let appendScrambledImages = function(element, object) {
+  let random = scramble(Object.keys(object));
+  for (let i = 0; i < random.length; i++) {
+    element.append(
+      $('<img>').attr('src', object[random[i]].img)
+    );
+  }
+}
+
 let loadStageOne = function() {
   $('#stage').empty()
     .append(stageTitle)
@@ -10,13 +19,7 @@ let loadStageOne = function() {
     .append(input)
     .append(submit);
   
-  let random = scramble(Object.keys(avatars));
-  for (let i = 0; i < random.length; i++) {
-    avatarSelection.append(
-      $('<img>').attr('src', avatars[random[i]].img)
-    );
-  }
-
+  appendScrambledImages(avatarSelection, avatars);
   stageTitle.text('Choose Your Champion');
   input.attr('placeholder', 'name your champion');
 };
