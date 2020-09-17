0dbe6214e8212493225ac3d9aba7d4b2f7ee8469
Travis Williams <williams44t@gmail.com>
2020-09-16 21:35:23 -0700

    added user div to second stage meant to host user info to left or above stage

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let options = $('<div></div>');
 let input = $('<input>');
 let submit = $('<button>Submit</button>');
 
+// User Elements
+let userTitle = $('<h2></h2>');
+let userImage = $('<img>');
+let user = $('<div></div>')
+  .append(userTitle)
+  .append(userImage);
+
 let appendScrambledImages = function(element, object) {
   element.empty();
   let random = scramble(Object.keys(object));
@@ -25,6 +32,8 @@ let loadStageOne = function() {
 };
 
 let loadStageTwo = function() {
+  $('#stage').before(user);
+  userTitle.html('userAvatar<br/>Champion for<br/>userName');
   appendScrambledImages(options,avatars);
   title.text('Choose Your Rival');
   input.remove();
